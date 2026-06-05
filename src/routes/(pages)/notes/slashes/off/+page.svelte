b601281af91ec2e783cb7d6ed5cafb41f4aedd20
temich <tema.gurtovoy@gmail.com>
2026-06-05 13:56:58 +0400

    feat: add `slashes` note

---
+++ src/routes/(pages)/notes/slashes/off/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+  import { page } from '$app/state'
+</script>
+
+<p>You're on <code class="text-sm">{page.url.pathname}</code>, this page has <code class="text-sm">trailingSlash</code> disabled.</p>
+<p>Note that the <code>← back</code> button at the bottom of the page (which is the same on all pages) is broken.</p>
